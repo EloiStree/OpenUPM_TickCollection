d5522a907cbaf0f28be228eb9da5cfd1a742b846
Éloi Strée <eloistree@gmail.com>
2024-08-10 04:17:31 +0200

    Save: 08/10/2024 4:17 AM

---
+++ Runtime/TickMono_OnMouseDownUp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TickMono_OnMouseDownUp : MonoBehaviour
+{
+    public UnityEvent m_onMouseDown;
+    public UnityEvent m_onMouseUp;
+
+    private void OnMouseDown()
+    {
+        m_onMouseDown.Invoke();
+    }
+    private void OnMouseUp()
+    {
+        m_onMouseUp.Invoke();
+    }
+
+}
