cef1ea90a0695913029874f784056ac503d0edcd
Eloi Strée <eloistree@gmail.com>
2024-08-16 02:34:50 +0200

    Save: 08/16/2024 2:34 AM

--- Editor/TickMono_AbstractDefaultEditor.cs
+++ Editor/TickMono_AbstractDefaultEditor.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 [CustomEditor(typeof(TickMono_AbstractDefault) )]
 public class TickMono_AbstractDefaultEditor : Editor
