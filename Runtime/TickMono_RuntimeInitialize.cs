c8f8cdef833d0027222afc397dd9c803969e1d94
Eloi Stree <eloistree@gmail.com>
2025-06-18 02:10:27 +0200

    Save: 2025_06_18_02_10

--- Runtime/TickMono_RuntimeInitialize.cs
+++ Runtime/TickMono_RuntimeInitialize.cs
@@ -4,32 +4,32 @@ namespace Eloi.Tick
 {
     public class TickMono_RuntimeInitialize : TickMono_AbstractDefault
     {
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
-        static void OnBeforeSplashScreen()
-        {
-            Debug.Log("Before SplashScreen is shown and before the first scene is loaded.");
-        }
+        //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        //static void OnBeforeSplashScreen()
+        //{
+        //    Debug.Log("Before SplashScreen is shown and before the first scene is loaded.");
+        //}
 
-        public UnityEvent m_onTickBeforeSceneLoad;
+        //public UnityEvent m_onTickBeforeSceneLoad;
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static void OnBeforeSceneLoad()
-        {
-            Debug.Log("First scene loading: Before Awake is called.");
+        //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        //static void OnBeforeSceneLoad()
+        //{
+        //    Debug.Log("First scene loading: Before Awake is called.");
             
-        }
+        //}
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-        static void OnAfterSceneLoad()
-        {
-            Debug.Log("First scene loaded: After Awake is called.");
-        }
+        //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        //static void OnAfterSceneLoad()
+        //{
+        //    Debug.Log("First scene loaded: After Awake is called.");
+        //}
 
-        [RuntimeInitializeOnLoadMethod]
-        static void OnRuntimeInitialized()
-        {
-            Debug.Log("Runtime initialized: First scene loaded: After Awake is called.");
-        }
+        //[RuntimeInitializeOnLoadMethod]
+        //static void OnRuntimeInitialized()
+        //{
+        //    Debug.Log("Runtime initialized: First scene loaded: After Awake is called.");
+        //}
     }
   
 
